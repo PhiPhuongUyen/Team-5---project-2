8584ce96ee62073252b68e923959b39464133b3f
Mao2003 <145522146+Mao2003@users.noreply.github.com>
2023-10-08 16:36:30 +0700

    Update SCATTERCHART_P2.py

--- SCATTERCHART_P2.py
+++ SCATTERCHART_P2.py
@@ -1,26 +1,13 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import plotly.express as px
 import pandas as pd
 import matplotlib.pyplot as plt
 from pandasql import sqldf
 import numpy as np
 
-
-# In[3]:
-
-
 url = "https://raw.githubusercontent.com/PhiPhuongUyen/Team-5/main/marketing_campaign.csv"
 df= pd.read_csv(url, sep="\t")
 pysqldf = lambda q: sqldf(q, globals())
 print(df)
-
-
-# In[6]:
 
 
 df1=pysqldf("SELECT ID, Year_Birth, Income, Dt_Customer, MntWines, MntFruits, MntMeatProducts, MntFishProducts, MntSweetProducts, MntGoldProds, NumDealsPurchases, NumWebPurchases, NumCatalogPurchases, NumStorePurchases, NumWebVisitsMonth\
@@ -29,23 +16,10 @@ df1=pysqldf("SELECT ID, Year_Birth, Income, Dt_Customer, MntWines, MntFruits, Mn
 print(df1)
 
 
-# In[ ]:
-
-
-
-
-
-# In[7]:
-
-
 df1["Spent"] = df1["MntWines"]+ df1["MntFruits"]+ df1["MntMeatProducts"]+ df1["MntFishProducts"]+ df1["MntSweetProducts"]+ df1["MntGoldProds"]
 df1=df1.rename(columns={"MntWines": "Wines","MntFruits":"Fruits","MntMeatProducts":"Meat","MntFishProducts":"Fish","MntSweetProducts":"Sweets","MntGoldProds":"Gold"})
 to_drop = ["ID"]
 df1 = df1.drop(to_drop, axis=1)
-
-
-# In[13]:
-
 
 dfscatters=df1[['Income','Spent']]
 plt.scatter(dfscatters['Income'], dfscatters['Spent'], color='orange', s=30, marker='*')
@@ -54,9 +28,6 @@ plt.ylabel("Spent")
 plt.xlabel("Income")
 plt.title("RELATIONSHIP BETWEEN REVENUES AND EXPENDITURES")
 plt.show()
-
-
-# In[ ]:
 
 
 
